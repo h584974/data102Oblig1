3e545cbde5817607bf9e6b7dc2340af15785b93e
olive <olive@192.168.0.107>
2020-01-25 16:14:57 +0100

    commit Fil

--- Oblig1/src/no/hvl/dat102/Fil.java
+++ Oblig1/src/no/hvl/dat102/Fil.java
@@ -19,22 +19,22 @@ public class Fil {
 			}
 			
 			int filmnummeri = tempstring.indexOf(SKILLE);
-			int filmnummer = Integer.parseInt(tempstring.substring(0, filmnummeri));
+			int filmnummer = Integer.parseInt(tempstring.substring(0, filmnummeri - 1));
 			
 			int titteli = tempstring.indexOf(SKILLE, filmnummeri);
-			String tittel = tempstring.substring(filmnummeri, titteli);
+			String tittel = tempstring.substring(filmnummeri, titteli - 1);
 			
 			int utgivelseaari = tempstring.indexOf(SKILLE, titteli);
-			int utgivelseaar = Integer.parseInt(tempstring.substring(titteli, utgivelseaari));
+			int utgivelseaar = Integer.parseInt(tempstring.substring(titteli, utgivelseaari - 1));
 			
 			int sjangeri = tempstring.indexOf(SKILLE, utgivelseaari);
-			Sjanger sjanger = Sjanger.finnSjanger(tempstring.substring(utgivelseaari, sjangeri));
+			Sjanger sjanger = Sjanger.finnSjanger(tempstring.substring(utgivelseaari, sjangeri - 1));
 			
 			int filmselskapi = tempstring.indexOf(SKILLE, sjangeri);
-			String filmselskap = tempstring.substring(sjangeri, filmselskapi);
+			String filmselskap = tempstring.substring(sjangeri, filmselskapi - 1);
 			
 			int produsenti = tempstring.indexOf(SKILLE, filmselskapi);
-			String produsent = tempstring.substring(filmselskapi, produsenti);	
+			String produsent = tempstring.substring(filmselskapi, produsenti - 1);	
 			
 			filmarkiv.leggTilFilm(new Film(filmnummer, produsent, tittel, utgivelseaar, sjanger, filmselskap));
 		}
