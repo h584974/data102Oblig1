90b5cb489abe7bd1d5eadbe9942c4ed5cf3f0fb7
olive <olive@158.37.229.131>
2020-01-28 13:46:20 +0100

    Commit Meny V1.0

--- Oblig1/src/no/hvl/dat102/Meny.java
+++ Oblig1/src/no/hvl/dat102/Meny.java
@@ -1,5 +1,6 @@
 package no.hvl.dat102;
 
+import no.hvl.dat102.adt.Filmarkiv;
 import no.hvl.dat102.adt.FilmarkivADT;
 import java.util.Scanner;
 import java.lang.NumberFormatException;
@@ -20,33 +21,41 @@ public class Meny {
 		System.out.println("Hva ønsker du å gjøre? Skriv følgende for ønskede funksjoner:");
 		
 		while(true) {
+			System.out.println();
 			System.out.println("Avslutt: 1");
 			System.out.println("Legg til ny film i samling: 2");
 			System.out.println("Slett film fra samling: 3");
 			System.out.println("Skriv ut statistikk om samling: 4");
-			System.out.println("Skriv ut film fra produsent: 5");
-			System.out.println("Skriv ut film fra tittel: 6");
+			System.out.println("Skriv ut filmer ut i fra produsent: 5");
+			System.out.println("Skriv ut film ut i fra tittel: 6");
+			System.out.println("Skriv ut alle filmer i akriv: 7");
 			
 			int svar = 1;
 			
 			try {
 				svar = Integer.parseInt(leser.nextLine());
 			} catch (NumberFormatException e) {
-				System.out.println("Ugyldig svar angitt\n");
+				System.out.println("Ugyldig svar angitt");
 			}
 			
 			if(svar == 1) {
 				break;
 			} else if(svar == 2) {
-				
+				filmarkiv.leggTilFilm(Tekstgrensesnitt.lesFilm());
 			} else if(svar == 3) {
-				
+				filmarkiv.slettFilm(Tekstgrensesnitt.lesFilm().getFilmnummer());
 			} else if(svar == 4) {
-				
+				Tekstgrensesnitt.skrivUtStatistikk(filmarkiv);
 			} else if(svar == 5) {
-				
+				System.out.println("Skriv produsent:");
+				Tekstgrensesnitt.skrivUtFilmTabell(filmarkiv.sokProdusent(leser.nextLine()));
+				System.out.println();
 			} else if(svar == 6) {
-				
+				System.out.println("Skriv tittel:");
+				Tekstgrensesnitt.skrivUtFilmTabell(filmarkiv.sokTittel(leser.nextLine()));
+				System.out.println();
+			} else if(svar == 7) {
+				Tekstgrensesnitt.skrivUtFilmTabell(filmarkiv.hentFilmTabell());
 			} else {
 				System.out.println("Ingen gyldig svar angitt\n");
 			}
